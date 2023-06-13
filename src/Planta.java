b1329b94d2afdfe057a967f123be0f09369e37dd
rm87085 <vp33435121@gmail.com>
2023-06-13 12:31:35 -0300

    Initial commit

---
+++ src/Planta.java
@@ -0,0 +1,12 @@
+public class Planta{
+//metodo construtor padrao
+    Planta(){
+}
+ ///sobrecarga do metodo contrutor
+    Planta(Semente semente){
+        //algoritimo
+    }
+
+    Planta(semente semente) {
+    }
+}
---
+++ src/solo.java
@@ -0,0 +1,11 @@
+public class solo {
+    boolean fertil;
+    String tipoSolo;
+    String lecalDoSono;
+    String estado;
+    //metodos
+    public Planta gerarPlanta(semente s1){
+        Planta p1 = new Planta (s1);
+        return p1;
+    }
+}
